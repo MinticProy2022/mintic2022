2921a81ba22a9aaa70a4b3c5086e8760af89182d
carivantelva15 <carivantelva@gmail.com>
2022-09-11 00:14:27 -0500

    se crea clase empleado,movimientoDinero,empresa servicio

--- src/main/java/com/sfyn/Ciclo3/entitis/Empresa.java
+++ src/main/java/com/sfyn/Ciclo3/entitis/Empresa.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 
 @Entity
 @Table(name="Empresa")
+
 public class Empresa {
    @Id
    @GeneratedValue(strategy = GenerationType.AUTO)
---
+++ src/main/java/com/sfyn/Ciclo3/services/EmpresaServices.java
@@ -0,0 +1,42 @@
+package com.sfyn.Ciclo3.services;
+
+
+import com.sfyn.Ciclo3.entitis.Empresa;
+import com.sfyn.Ciclo3.repositorio.EmpresaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmpresaServices {
+    @Autowired  //conectamos esta clase con el repositorio de empresa
+
+    EmpresaRepository empresaRepository;  //creamos un objeto tipo empresaRepositorio para poder usar los metodos que dicha clase hereda
+
+    //Metodo que retornara la lista de empresas usando metodos heredados del jpaRepository
+    public List<Empresa> getAllEmpresas(){
+        List<Empresa>empresaList =new ArrayList<>();
+        empresaRepository.findAll().forEach(empresa -> empresaList.add(empresa)); //recorremos la interable que regresa el metodo FindAll del jpa y los guardamos en la lista
+        return empresaList;
+    }
+
+    //metodo que trae un objeto de tipo empresa cuando cuento con el id de la misma
+    public Empresa getEmpresaById(Integer id){
+        return empresaRepository.findById(id).get();
+    }
+
+    //Metodo para guardar o actualizar objetos de tipo Empresa
+    public boolean saveOrUpdateEmpresa(Empresa empresa){
+        Empresa emp=empresaRepository.save(empresa);
+        if(empresaRepository.findById(emp.getId())!=null){
+            return  true;
+        }
+        return false;
+    }
+
+
+
+}
+
