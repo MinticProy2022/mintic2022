77eaff97cf35055727e546fc83642fefa63b62e3
carivantelva15 <carivantelva@gmail.com>
2022-09-13 20:36:42 -0500

    se efectua carga de datos desde html con boton de agregar

--- src/main/java/com/sfyn/Ciclo3/controller/Controlador.java
+++ src/main/java/com/sfyn/Ciclo3/controller/Controlador.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import java.util.List;
 
 //@RestController
@@ -17,19 +20,28 @@ public class Controlador {
 
     @GetMapping({"/", "/verEmpresas"})
     public String viewEmpresas(Model model) {
-        List<Empresa>listaEmpresas=empresaService.getAllEmpresas();
-     model.addAttribute("emplist", listaEmpresas);
+        List<Empresa> listaEmpresas = empresaService.getAllEmpresas();
+        model.addAttribute("emplist", listaEmpresas);
         return "mostrarEmpresas";    //este es el nombre para ver en html
 
     }
+
     @GetMapping("/AgregarEmpresa")
-    public  String nuevaEmpresa(Model model){
-        Empresa emp= new Empresa();
-        model.addAttribute("emp",emp);
+    public String nuevaEmpresa(Model model) {
+        Empresa emp = new Empresa();
+        model.addAttribute("emp", emp);
         return "agregarEmpresa";
 
     }
 
+    @PostMapping("/GuardarEmpresa")
+    public String guardarEmpresa(Empresa emp, RedirectAttributes redirectAttributes) {
+        if (empresaService.saveOrUpdateEmpresa(emp) == true) {
+            redirectAttributes.addFlashAttribute("mensaje", "saveOK");
+            return "redirect:/VerEmpresas";
+        }
+        redirectAttributes.addFlashAttribute("mensaje", "saveError");
+        return "redirect:/AgregarEmpresa";
+    }
+
 }
-
-
