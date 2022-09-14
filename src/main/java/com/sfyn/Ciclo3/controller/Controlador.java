9423aa8852e1bd9327080e0a38c2204e5b1ff8dd
carivantelva15 <carivantelva@gmail.com>
2022-09-14 07:25:17 -0500

    se ingresa imagen con url en la parte inferio de nombre lista deempresas

--- src/main/java/com/sfyn/Ciclo3/controller/Controlador.java
+++ src/main/java/com/sfyn/Ciclo3/controller/Controlador.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -41,7 +43,18 @@ public class Controlador {
             return "redirect:/verEmpresas";
         }
         redirectAttributes.addFlashAttribute("mensaje", "saveError");
-        return "redirect:/mostrarEmpresa";
+        return "redirect:/mostrarEmpresa"; //aqui hace retorno a la lista de empresas pasando por el html
     }
+
+    @GetMapping("/EditarEmpresa/{}")
+    public String editarEmpresa(Model model, @PathVariable Integer id, @ModelAttribute("mensaje") String mensaje){
+        Empresa emp=empresaService.getEmpresaById(id);
+        //Creamos un atributo para el modelo, que se llame igualmente emp y es el que ira al html para llenar o alimentar campos
+        model.addAttribute("emp",emp);
+        model.addAttribute("mensaje", mensaje);
+        return "editarEmpresa";
+    }
+
+
 
 }
